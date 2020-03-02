1d48a5c8d1de2974816bf86953cff830b64a0d7c
Heini Natri <heini.natri@gmail.com>
2020-03-01 17:06:04 -0700

    Update run_bam_processing_snakemake.sh

--- run_bam_processing_snakemake.sh
+++ run_bam_processing_snakemake.sh
@@ -13,4 +13,4 @@ source activate nasonia_environment
 
 export PERL5LIB=/packages/6x/vcftools/0.1.12b/lib/perl5/site_perl
 
-snakemake --snakefile process_gir_bams.snakefile -j 20 --rerun-incomplete --cluster "sbatch -n 1 --nodes 1 -c 8 -t 96:00:00"
+snakemake --snakefile process_bams.snakefile -j 20 --rerun-incomplete --cluster "sbatch -n 1 --nodes 1 -c 8 -t 96:00:00"
